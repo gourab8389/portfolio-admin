2c7bb2d1145356f43a42b9a621058f335c23da8c
gourab8389 <deyg6988@gmail.com>
2025-10-15 23:11:08 +0530

    refactor: improve formatting and structure of Providers component

--- src/components/layout/providers.tsx
+++ src/components/layout/providers.tsx
@@ -1,43 +1,46 @@
-"use client"
+"use client";
 
-import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ThemeProvider } from "next-themes"
-import { PropsWithChildren, useState } from "react"
-import { NuqsAdapter } from 'nuqs/adapters/next/app';
-import ToasterNextTheme from "../shared/toast-provider"
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
+import { PropsWithChildren, useState } from "react";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import ToasterNextTheme from "../shared/toast-provider";
 
 export const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          onError: (err) => {
+            let errorMessage: string;
+            if (err instanceof Error) {
+              errorMessage = err.message;
+            } else {
+              errorMessage = "An unknown error occurred.";
+            }
+            console.log(errorMessage);
+          },
+        }),
+      })
+  );
 
-    const [queryClient] = useState(
-        () =>
-            new QueryClient({
-                queryCache: new QueryCache({
-                    onError: (err) => {
-                        let errorMessage: string
-                        if (err instanceof Error) {
-                            errorMessage = err.message
-                        } else {
-                            errorMessage = "An unknown error occurred."
-                        }
-                        console.log(errorMessage)
-                    },
-                }),
-            })
-    )
-
-    return <QueryClientProvider client={queryClient}>
-        <NuqsAdapter>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="light"
-                enableSystem
-                disableTransitionOnChange
-
-            >
-                {children}
-                <ToasterNextTheme />
-            </ThemeProvider>
-        </NuqsAdapter>
-
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NuqsAdapter>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <ToasterNextTheme />
+        </ThemeProvider>
+      </NuqsAdapter>
     </QueryClientProvider>
-}
+  );
+};
