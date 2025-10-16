81e554f30d1f2011cafad214cf47b53e0ca208e0
Gourab Dey <143871622+gourab8389@users.noreply.github.com>
2025-10-16 22:58:32 +0530

    Update DashboardPage styling and structure

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DashboardPage = () => {
   return (
-    <div>
+    <div className="w-full p-5 rounded-md border flex flex-col gap-5">
       dashboard page
     </div>
   )
