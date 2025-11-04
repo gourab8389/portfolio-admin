b996ae155bdfbc8bf415e8628d1fa255816b8f1c
gourab8389 <deyg6988@gmail.com>
2025-11-04 23:59:11 +0530

    feat: create SkillPage component for managing education details in the dashboard

---
+++ src/app/(routes)/dashboard/profiles/skills/page.tsx
@@ -0,0 +1,27 @@
+import Heading from "@/components/heading";
+import PageContainer from "@/components/layout/page-container";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+export const metadata = {
+  title: "Skill Page | Dashboard",
+};
+
+const SkillPage = async () => {
+  return (
+    <PageContainer scrollable={true}>
+      <div className="flex flex-1 flex-col space-y-4">
+        <div className="flex items-start justify-between">
+          <Heading
+            title="Education Details"
+            description="Manage your education profile information"
+          />
+        </div>
+        <Separator />
+        {/* <SkillForm /> */}
+      </div>
+    </PageContainer>
+  );
+};
+
+export default SkillPage;
